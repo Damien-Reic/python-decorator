a214841988c3c7ed54eb9f47e5eee4ffa4f18ad1
Damien Reichhart <112109119+Damien-Reic@users.noreply.github.com>
2022-12-23 10:50:15 +0100

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -15,14 +15,14 @@ def decorator_with_log(func):
     """A decorator that wraps a function, return the original function result and write all of this in a log file with more information about the runtime
     """
     from datetime import datetime
-    import time
-    import os
+    from time import time
+    from os import getcwd
     def wrapper(*args, **kwargs):
-        with open(os.getcwd() + '/' + "logs.txt", "a") as f:     # open the log file
-            start = (datetime.now(),time.time())                    # defines the time at which the program starts to run
+        with open(getcwd() + '/' + "logs.txt", "a") as f:     # open the log file
+            start = (datetime.now(),time())                    # defines the time at which the program starts to run
             print("started")
             value = func(*args, **kwargs)               #get the func result
-            f.write(f"Function '{func.__name__}' called with {[arg for arg in args]} at {start[0]}  return {value} and done in {time.time() - start[1]} seconds" + "\n") #write the informations in the logs.txt file
+            f.write(f"Function '{func.__name__}' called with {[arg for arg in args]} at {start[0]}  return {value} and done in {time() - start[1]} seconds" + "\n") #write the informations in the logs.txt file
             print("done")
             return value    # return the func result
     return wrapper
@@ -35,5 +35,3 @@ if __name__ == "__main__":     #Example to test
     def f(*args):
         print("hello world")
     f(1,8,5,6)
-
-    
